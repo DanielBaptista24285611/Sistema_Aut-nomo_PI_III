ea515740b6c9690162d65e823b940b23845aa00e
Isaias_Maia2024 <168050027+IsaiasdeOliveira@users.noreply.github.com>
2025-03-06 17:35:40 -0300

    Add files via upload

--- Sistema_Autônomo_PI_III/Form1.cs
+++ Sistema_Autônomo_PI_III/Form1.cs
@@ -11,7 +11,7 @@ using KingMeServer;
 
 namespace Sistema_Autônomo_PI_III
 {
-    public partial class Form1: Form
+    public partial class Form1 : Form
     {
         public Form1()
         {
@@ -19,7 +19,7 @@ namespace Sistema_Autônomo_PI_III
         }
         private void btnCriar_Click(object sender, EventArgs e)
         {
-            Jogo.CriarPartida($"{txtNomePartida.Text}", $"{txtSenhaPartida.Text}", $"{txtNomeGrupo.Text}");
+            string partida = Jogo.CriarPartida($"{txtNomePartida.Text}", $"{txtSenhaPartida.Text}", $"{txtNomeGrupo.Text}");
 
             lblInforma.Text = "Partida Criada!";
         }
@@ -33,14 +33,14 @@ namespace Sistema_Autônomo_PI_III
         private void btnListarPartidas_Click(object sender, EventArgs e)
         {
             string retorno = Jogo.ListarPartidas("T");
-    
 
-            retorno = retorno.Replace("\r","");
+
+            retorno = retorno.Replace("\r", "");
             retorno = retorno.Substring(0, retorno.Length - 1);
             string[] partidas = retorno.Split('\n');
 
 
-            for(int i = 0; i < partidas.Length; i++)
+            for (int i = 0; i < partidas.Length; i++)
             {
                 listBox1.Items.Add(partidas[i]);
 
@@ -53,8 +53,67 @@ namespace Sistema_Autônomo_PI_III
             string partida = listBox1.ToString();
             string[] dadosPartida = partida.Split('\n');
 
-            
+
         }
 
+        private void btnListarJogadores_Click(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedItem == null)
+            {
+                MessageBox.Show("Por favor, selecione uma partida.");
+                return;
+            }
+
+            string partida = listBox1.SelectedItem.ToString();
+            string[] dadosPartida = partida.Split('\n');
+
+            int idPartida = Convert.ToInt32(dadosPartida[0]);
+
+            string retornoJ = Jogo.ListarJogadores(idPartida);
+            retornoJ = retornoJ.Replace("\r", "");
+            string[] jogadores = retornoJ.Split('\n');
+
+            listBox2.Items.Clear(); 
+            foreach (string jogador in jogadores)
+            {
+                listBox2.Items.Add(jogador);
+            }
+        }
+
+        private void btnEntrar_Click(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedItem == null)
+            {
+                MessageBox.Show("Por favor, selecione uma partida.");
+                return;
+            }
+
+            string partida = listBox1.SelectedItem.ToString();
+            string[] dadosPartida = partida.Split(',');
+            
+
+            if (dadosPartida.Length == 0 || string.IsNullOrWhiteSpace(dadosPartida[0]))
+            {
+                MessageBox.Show("Os dados da partida estão vazios ou incorretos.");
+                return;
+            }
+            
+            int idPartida = Convert.ToInt32(dadosPartida[0]);
+         
+
+            string nomeJogador = txtNomeJogador.Text;
+            string senhaPartida = txtSenhaPartida.Text;
+
+            string jogador = Jogo.Entrar(idPartida, nomeJogador, senhaPartida);
+
+            if (string.IsNullOrEmpty(jogador))
+            {
+                MessageBox.Show("Erro ao entrar na partida. Verifique suas credenciais.");
+            }
+            else
+            {
+                MessageBox.Show($"Jogador {jogador} entrou na partida com sucesso.");
+            }
+        }
     }
 }
