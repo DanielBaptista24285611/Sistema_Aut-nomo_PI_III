b1abd614f2400a3b745635956d16a7538906f87d
Isaias_Maia_de_Oliveira <168050027+IsaiasdeOliveira@users.noreply.github.com>
2025-03-23 19:01:02 -0300

    Update Geral Final

---
+++ Sistema_Autônomo_PI_III/Sistema_Autônomo_PI_III/Menu.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace Sistema_Autônomo_PI_III
+{
+    partial class Menu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Menu));
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnLobby = new System.Windows.Forms.Button();
+            this.btnSair = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.label1.Font = new System.Drawing.Font("Algerian", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(224, 145);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(290, 22);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "CAVALEIROS DE CANTERBURY";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // btnLobby
+            // 
+            this.btnLobby.Font = new System.Drawing.Font("Impact", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLobby.Location = new System.Drawing.Point(283, 269);
+            this.btnLobby.Name = "btnLobby";
+            this.btnLobby.Size = new System.Drawing.Size(167, 47);
+            this.btnLobby.TabIndex = 1;
+            this.btnLobby.Text = "Iniciar";
+            this.btnLobby.UseVisualStyleBackColor = true;
+            this.btnLobby.Click += new System.EventHandler(this.btnLobby_Click);
+            // 
+            // btnSair
+            // 
+            this.btnSair.Font = new System.Drawing.Font("Impact", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSair.Location = new System.Drawing.Point(283, 361);
+            this.btnSair.Name = "btnSair";
+            this.btnSair.Size = new System.Drawing.Size(167, 47);
+            this.btnSair.TabIndex = 2;
+            this.btnSair.Text = "Sair";
+            this.btnSair.UseVisualStyleBackColor = true;
+            this.btnSair.Click += new System.EventHandler(this.btnSair_Click);
+            // 
+            // Menu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Sistema_Autônomo_PI_III.Properties.Resources.fundo_Menu;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(699, 575);
+            this.Controls.Add(this.btnSair);
+            this.Controls.Add(this.btnLobby);
+            this.Controls.Add(this.label1);
+            this.DoubleBuffered = true;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Menu";
+            this.Text = "Cavaleiros de Canterbury";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnLobby;
+        private System.Windows.Forms.Button btnSair;
+    }
+}
