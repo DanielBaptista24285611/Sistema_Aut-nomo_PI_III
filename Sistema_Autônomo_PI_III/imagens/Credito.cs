2886bd2bc679be2295f76a9b5b466eb8d0461299
Isaias_Maia_de_Oliveira <168050027+IsaiasdeOliveira@users.noreply.github.com>
2025-06-05 21:12:36 -0300

    Add files via upload

---
+++ Sistema_Autônomo_PI_III/imagens/Credito.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sistema_Autônomo_PI_III
+{
+    public partial class Credito : Form
+    {
+        public Credito()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Menu menu = new Menu(); // Cria uma instância do formulário Menu
+            menu.Show(); // Exibe o formulário Menu
+        }
+    }
+}
